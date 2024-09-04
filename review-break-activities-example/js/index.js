82d293ec300dbc4b8c288610525afde05eca8984
Tom Maurer <tmaurer@nait.ca>
2024-09-04 14:50:45 -0600

    day 2

--- review-break-activities-example/js/index.js
+++ review-break-activities-example/js/index.js
@@ -6,3 +6,32 @@ HTML for list topic list item
     NEW TOPIC HERE
 </li>
 */
+
+let topicList = document.querySelector(".topics-list");
+let topicForm = document.querySelector(".new-topic-form");
+
+console.log(topicList);
+console.log(topicForm);
+
+const addTopicToPage = function (topicName, theTopicList) {
+    let newTopicElement = `<li class="list-group-item">
+        ${topicName}
+    </li>`;
+    theTopicList.innerHTML += newTopicElement;
+}
+
+const eventHandler = function (event) {
+    event.preventDefault();
+
+    let input = event.target.elements["new-topic"];
+    let inputValue = input.value;
+
+    if (inputValue === "") {
+        input.classList.add("is-invalid");
+    } else {
+        input.classList.remove("is-invalid");
+        addTopicToPage(inputValue, topicList);
+    }
+}
+
+topicForm.addEventListener("submit", eventHandler);
